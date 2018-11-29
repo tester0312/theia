959d83c27a3247b76189a25db4c229a9df85f16e
Oleksii Orel <oorel@redhat.com>
2018-11-29 11:00:24 +0200

    implement setIconPath for webview
    Signed-off-by: Oleksii Orel <oorel@redhat.com>

--- packages/plugin/src/theia.d.ts
+++ packages/plugin/src/theia.d.ts
@@ -2419,8 +2419,8 @@ declare module '@theia/plugin' {
         title: string;
 
         /**
-		 * Icon for the panel shown in UI.
-		 */
+         * Icon for the panel shown in UI.
+         */
         iconPath?: Uri | { light: Uri; dark: Uri };
 
         /**
